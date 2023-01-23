fa4d6a7010255e11123506111007fa9cb1c85e10
Torben Kalkhof <kalkhof@esa.tu-darmstadt.de>
2023-01-23 13:54:49 +0100

    Initial version
    This header provides a wrapper for TaPaSCo tasks to be used in HPX.

---
+++ Tapasco-HPXConfig.cmake
@@ -0,0 +1,3 @@
+project("Tapasco-HPX")
+add_library(Tapasco-HPX INTERFACE)
+target_include_directories(Tapasco-HPX INTERFACE "${CMAKE_CURRENT_LIST_DIR}/include")
---
+++ include/tapasco-hpx.hpp
@@ -0,0 +1,49 @@
+#ifndef TAPASCO_HPX_WRAPPER_TAPASCO_HPX_HPP
+#define TAPASCO_HPX_WRAPPER_TAPASCO_HPX_HPP
+
+#include <tapasco.hpp>
+#include <hpx/thread.hpp>
+
+using namespace tapasco;
+
+template <typename R, typename... Targs>
+void tapasco_hpx_job(Tapasco const *tapasco, PEId peid, RetVal<R> &ret, Targs... args) {
+	JobFuture job;
+	while (true) {
+		if (tapasco->try_launch(job, peid, ret, args...)) {
+			hpx::this_thread::suspend();
+			continue;
+		}
+		break;
+	}
+
+	while (true) {
+		if (job(false)) {
+			hpx::this_thread::suspend();
+			continue;
+		}
+		break;
+	}
+}
+
+template <typename... Targs>
+void tapasco_hpx_job(Tapasco *tapasco, PEId peid, Targs... args) {
+	JobFuture job;
+	while (true) {
+		if (tapasco->try_launch(job, peid, args...)) {
+			hpx::this_thread::suspend();
+			continue;
+		}
+		break;
+	}
+
+	while (true) {
+		if (job(false)) {
+			hpx::this_thread::suspend();
+			continue;
+		}
+		break;
+	}
+}
+
+#endif //TAPASCO_HPX_WRAPPER_TAPASCO_HPX_HPP
