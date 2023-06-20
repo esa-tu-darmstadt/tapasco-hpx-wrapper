4eac5b8d0d7c1af0845e37187d7039b11b148560
Torben Kalkhof <kalkhof@esa.tu-darmstadt.de>
2023-06-20 10:10:58 +0200

    Use references instead of pointers for Tapasco object. Closes #1.

--- include/tapasco-hpx.hpp
+++ include/tapasco-hpx.hpp
@@ -7,10 +7,10 @@
 using namespace tapasco;
 
 template <typename R, typename... Targs>
-void tapasco_hpx_job(Tapasco const *tapasco, PEId peid, RetVal<R> &ret, Targs... args) {
+void tapasco_hpx_job(Tapasco const &tapasco, PEId peid, RetVal<R> &ret, Targs... args) {
 	JobFuture job;
 	while (true) {
-		if (tapasco->try_launch(job, peid, ret, args...)) {
+		if (tapasco.try_launch(job, peid, ret, args...)) {
 			hpx::this_thread::suspend();
 			continue;
 		}
@@ -27,10 +27,10 @@ void tapasco_hpx_job(Tapasco const *tapasco, PEId peid, RetVal<R> &ret, Targs...
 }
 
 template <typename... Targs>
-void tapasco_hpx_job(Tapasco *tapasco, PEId peid, Targs... args) {
+void tapasco_hpx_job(Tapasco &tapasco, PEId peid, Targs... args) {
 	JobFuture job;
 	while (true) {
-		if (tapasco->try_launch(job, peid, args...)) {
+		if (tapasco.try_launch(job, peid, args...)) {
 			hpx::this_thread::suspend();
 			continue;
 		}
